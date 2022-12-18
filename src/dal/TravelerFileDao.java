b3fe468b3cdd89bf00fb94407f9816603b6200de
ohaddgani123 <dganiohad@gmail.com>
2022-12-18 13:29:25 +0200

    int

--- src/dal/TravelerFileDao.java
+++ src/dal/TravelerFileDao.java
@@ -10,7 +10,7 @@ import entitys.Traveler;
 @Component
 public class TravelerFileDao implements FileDao<Traveler>{
 	
-	private int l;
+	private int n;
 
 	@Override
 	public List<Traveler> getAll() throws Exception {
