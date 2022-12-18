e2b3095bacb4b4a8326829fe85c8583cb71432f0
PBar24 <crhkv123@gmail.com>
2022-12-18 23:20:44 +0200

    service

--- src/service/TravelerService.java
+++ src/service/TravelerService.java
@@ -1,5 +1,7 @@
 package service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,4 +15,23 @@ public class TravelerService {
 	@Qualifier()//TravelerFileDao
 	@Autowired
 	private FileDao<Traveler> dependency;
+	
+	public List<Traveler> getAll() throws Exception {
+		return dependency.getAll();
+	}
+
+	public void save(Traveler t) throws Exception {
+		dependency.save(t);
+	}
+
+	public void update(Traveler t, int flightId) throws Exception {
+	}
+
+	public void delete(int id) throws Exception {
+		
+	}
+
+	public Traveler get(int id) throws Exception {
+		return null;
+	}
 }
