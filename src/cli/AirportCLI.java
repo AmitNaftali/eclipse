a6025e14f67cbabc6d47e289cd71a75ee067426f
amit <amittnn1823@gmail.com>
2022-12-20 20:50:36 +0200

    half cli

--- src/cli/AirportCLI.java
+++ src/cli/AirportCLI.java
@@ -1,9 +1,19 @@
 package cli;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class AirportCLI {
+import entitys.Flight;
+import entitys.Traveler;
+import exceptions.FlightNotFoundException;
+import exceptions.FullFlightException;
+import exceptions.TravelerAlredyExistsException;
+import service.TravelerService;
 
+public class AirportCLI {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//show flight
@@ -15,7 +25,108 @@ public class AirportCLI {
 		// Load the Spring configuration file
 
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		Scanner sc = new Scanner(System.in);  // Create a Scanner object
+		TravelerService service = new TravelerService();
+		
+		int decision = sc.nextInt();// Read user input
+		while(printAllDestinations(service))
+		{
+			decision = sc.nextInt();
+		}
+		
 		context.close();
 	}
+
+	public static boolean actions(int decision,TravelerService service,Flight dest) {
+		Scanner sc = new Scanner(System.in);
+		switch(decision) {
+		case 1:
+			printAllDestinations(service);
+			break;
+		case 2:
+			printFlight(service,sc);
+			break;
+		case 3:
+			addTraveler(service,dest,sc);
+			break;
+		case 4:
+			System.out.println();
+			break;
+		case 5:
+			System.out.println();
+			break;
+		case 0:
+			System.out.println("Exited.");
+			return false;
+		}	
+		return true;
+	}
+	
+
+	public static void printFlight(TravelerService service,Scanner sc) {
+		try {
+			int id = sc.nextInt();
+			System.out.println(service.get(id).toString());
+		}
+		catch(FlightNotFoundException fnfe)
+		{
+			System.out.println("Flight not found.");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void addTraveler(TravelerService service,Flight dest,Scanner sc) {
+		try {
+			System.out.println("Enter full name:");
+			String name = sc.nextLine();
+			System.out.println("Enter passport id:");
+			int passportId = sc.nextInt();
+			Traveler newTraveler = new Traveler(passportId,name);
+			service.addTravelerToFlight(dest.getId(), newTraveler);
+		}catch(FlightNotFoundException fnfe)
+		{
+			System.out.println("flight not found");
+		}
+		catch(TravelerAlredyExistsException taee)
+		{
+			System.out.println("traveler already exist not found");
+		}
+		catch(FullFlightException ffe)
+		{
+			System.out.println("flight is full of travelers");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public static void removeTraveler(TravelerService service,Scanner sc) {
+		
+	}
+	public static void deleteTraveler(TravelerService service,Scanner sc) {
+		
+	}
+
+	public static boolean printAllDestinations(TravelerService service)
+	{
+		try {
+			List<String> dests = service.showDestinations();
+			Scanner sc = new Scanner(System.in);
+			String dest = sc.nextLine();
+			List<Flight> flights = service.showFlightsToDestinations(dest);
+			int flightNum = sc.nextInt();
+			Flight chosenFlight = flights.get(flightNum -1);
+			System.out.println("actions: 1 - add to flight, 2 - remove from flight, 3 - show all flights, 0 - exit");
+			int decision = sc.nextInt();
+			return actions(decision,service,chosenFlight);
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
 
 }
